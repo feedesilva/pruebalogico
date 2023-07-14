cafe20de442a10be603f423beb4517237a9ec340
feedesilva <f.nahuelsilva@gmail.com>
2023-07-13 22:34:44 -0300

    init

---
+++ prueba.pl
@@ -0,0 +1,11 @@
+% Cancion, Compositores,  Reproducciones
+cancion(bailanSinCesar, [pabloIlabaca, rodrigoSalinas], 10600177).
+cancion(yoOpino, [alvaroDiaz, carlosEspinoza, rodrigoSalinas], 5209110).
+cancion(equilibrioEspiritual, [danielCastro, alvaroDiaz, pabloIlabaca, pedroPeirano, rodrigoSalinas], 12052254).
+cancion(tangananicaTanganana, [danielCastro, pabloIlabaca, pedroPeirano], 5516191).
+cancion(dienteBlanco, [danielCastro, pabloIlabaca, pedroPeirano], 5872927). 
+
+cancion(lala, [pabloIlabaca, pedroPeirano], 5100530).
+
+cancion(meCortaronMalElPelo, [danielCastro, alvaroDiaz, pabloIlabaca, rodrigoSalinas], 3428854).
+
